a8c33bb2108ebb87af51cb96e82ef6924cc6676f
Aaden Sah <deltatechaadensah@gmail.com>
2018-03-27 17:33:08 +0545

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { ContactusPage } from '../pages/contactus/contactus';
@@ -27,7 +26,6 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
-    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
